974ed20a4b5911b04f86ab9c3dd374d2848b5bf6
Logan <Ljcastelloe@uncg.edu>
2026-03-06 20:25:22 -0500

    Intitial Spring Boot project files

---
+++ src/main/java/com/Logan/Assignment3CRUDAPI/Assignment3CrudapiApplication.java
@@ -0,0 +1,13 @@
+package com.Logan.Assignment3CRUDAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment3CrudapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment3CrudapiApplication.class, args);
+	}
+
+}
